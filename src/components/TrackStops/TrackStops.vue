c565c9f1c98556612973d2ef5d8f2c9e022d0e21
Yukio Ueda <52945670+Yukio0315@users.noreply.github.com>
2019-11-07 22:33:10 +0900

    wrote trackstops's vue template and css

---
+++ src/components/TrackStops/TrackStops.vue
@@ -0,0 +1,19 @@
+<template src="./template.html">
+</template>
+
+<script>
+export default {
+  name: "TrackStop",
+  props: {
+    hit: Number,
+    address: String,
+    city: String,
+    state: String,
+    zip: String,
+    tell: String,
+  },
+};
+</script>
+
+<style scoped src="./style.css">
+</style>
