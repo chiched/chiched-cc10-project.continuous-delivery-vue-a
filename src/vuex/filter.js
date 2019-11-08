5868b9d68ce93d26a314446ffb419b2bd6029205
David Chiche <davidchiche@gmail.com>
2019-11-08 14:32:14 +0900

    Updated store

--- src/vuex/filter.js
+++ src/vuex/filter.js
@@ -1,53 +1,98 @@
-//import axios from "axios";
+import axios from "axios";
 
 export default {
-    state: {
-      states: ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"],
-      locationInformation: [],
-      currentView: "filterPanel",
-      currentFilters: {
-        state: "",
-        city: "",
-        highway: "",
-        oilchange: true,
-        lightMechanical: true,
-        tirepass: true,
-        truckTireCare: true,
-        travelShop,
-        countryStore,
-        atm: true,
-        wifi: true,
-        others: true,
-        arbys: true,
-        wendys: true
-    },
-    getters: {
-
+  state: {
+    states: [
+      "AL",
+      "AK",
+      "AZ",
+      "AR",
+      "CA",
+      "CO",
+      "CT",
+      "DE",
+      "DC",
+      "FL",
+      "GA",
+      "HI",
+      "ID",
+      "IL",
+      "IN",
+      "IA",
+      "KS",
+      "KY",
+      "LA",
+      "ME",
+      "MD",
+      "MA",
+      "MI",
+      "MN",
+      "MS",
+      "MO",
+      "MT",
+      "NE",
+      "NV",
+      "NH",
+      "NJ",
+      "NM",
+      "NY",
+      "NC",
+      "ND",
+      "OH",
+      "OK",
+      "OR",
+      "PA",
+      "RI",
+      "SC",
+      "SD",
+      "TN",
+      "TX",
+      "UT",
+      "VT",
+      "VA",
+      "WA",
+      "WV",
+      "WI",
+      "WY",
+    ],
+    locationInformation: [],
+    currentView: "filterPanel",
+    currentFilters: {
+      state: "",
+      city: "",
+      highway: "",
     },
     mutations: {
       setLocationInformation(state, locationInformation) {
         state.locationInformation = locationInformation;
       },
       switchView(state, view) {
-          state.currentView = view;
+        state.currentView = view;
       },
-      updateFilters(state, newFilters) { 
+      updateFilters(state, newFilters) {
         for (let filter in newFilters) {
           state.currentFilters[filter] = newFilters[filter];
         }
-       //? return state.currentFilters;
-       }
+      },
     },
     actions: {
-        setLocationInformationswitchView(locationInformation) {
-            store.commit("setLocationInformationswitchView", locationInformation);
-        },
-        switchView(view) {
-            store.commit("switchView", view);
-        },
-        updateFilters(newFilters) {
-            store.commit("updateFilters", newFilters);
+      async getFilteredLocations(store, filterOptions) {
+        store.commit("updateFilters", filterOptions);
+        try {
+          const data = await axios.get("/api/locations");
+          const filteredLocationInformation = data.filter((location) => {
+            let allTrue = true;
+            for (let [key, value] in Object.entries(filterOptions)) {
+              location[key] === value ? true : (allTrue = false);
+            }
+            return allTrue;
+          });
+          store.commit("setLocationInformation", filteredLocationInformation);
+          store.commit("switchView", "resultsPanel");
+        } catch (err) {
+          console.log(err);
         }
-    }
-  }
-}
+      },
+    },
+  },
+};
