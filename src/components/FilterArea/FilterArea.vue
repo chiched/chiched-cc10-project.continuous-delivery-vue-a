d665f481304258b521a9329b4b3febe3a97ab790
nouvelle <nouvellenuts@gmail.com>
2019-11-08 16:12:20 +0900

    inserted state data into dropdown from state(vuex)

--- src/components/FilterArea/FilterArea.vue
+++ src/components/FilterArea/FilterArea.vue
@@ -1,11 +1,15 @@
-<template src="./template.html">
-</template>
+<template src="./template.html"> </template>
 
 <script>
 export default {
   name: "FilterArea",
   props: {
     msg: String,
+  },
+  computed: {
+    states() {
+      return this.$store.state.filter.states;
+    },
   },
 };
 </script>
