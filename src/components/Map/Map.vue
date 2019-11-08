bf293829db6c393c66273ef78b9266d0fa83d034
nouvelle <nouvellenuts@gmail.com>
2019-11-08 14:18:13 +0900

    redo: template

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -1,21 +1,4 @@
-<template>
-  <div id="map">
-    <GmapMap
-      :zoom="5"
-      :center="{ lat: 25.7392, lng: -104.9903 }"
-      map-type-id="terrain"
-      style="width: 100%; height: 300px"
-    >
-      <GmapMarker
-        v-for="location in locations"
-        :key="location.key"
-        :position="location.position"
-        :animation="location.defaultAnimation"
-        @rightclick="markerRightClicked"
-      />
-    </GmapMap>
-  </div>
-</template>
+<template src="./template.html"></template>
 
 <script>
 import { gmapApi } from "vue2-google-maps";
